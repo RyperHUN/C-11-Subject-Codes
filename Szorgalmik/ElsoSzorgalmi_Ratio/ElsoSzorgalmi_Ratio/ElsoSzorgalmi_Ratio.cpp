895c52f8b97733ee6b72dba6e0b328d4caf2d082
RyperHUN <akormanyzsolt@gmail.com>
2016-09-06 21:05:54 +0200

    SZorgalmi1 - Added *, /, +=, +, >>

--- Szorgalmik/ElsoSzorgalmi_Ratio/ElsoSzorgalmi_Ratio/ElsoSzorgalmi_Ratio.cpp
+++ Szorgalmik/ElsoSzorgalmi_Ratio/ElsoSzorgalmi_Ratio/ElsoSzorgalmi_Ratio.cpp
@@ -14,6 +14,56 @@ public:
 private:
 	int num_;       /* numerator */
 	int den_;       /* denominator */
+
+public:
+	Ratio operator+(Ratio r1)
+	{
+		return Ratio(
+			num_ * r1.den() + r1.num() * den_,
+			r1.den() * den_);
+	}
+	Ratio& operator+=(Ratio r1)
+	{
+		*this = *this + r1;
+		return *this;
+	}
+	///Egyoperandusu -
+	//Ratio operator-(Ratio r1)
+	//{
+	//	return Ratio(
+	//		num_ * r1.den() + r1.num() * den_,
+	//		r1.den() * den_);
+	//}
+	///-=
+	///*
+	Ratio operator*(Ratio r1)
+	{
+		return Ratio (num_ * r1.num (), den_ * r1.den ());
+	}
+	///*=
+	///  /
+	Ratio operator/(Ratio r1)
+	{
+		return Ratio(num_ * r1.den(), den_ * r1.num());
+	}
+	///  /=
+	
+
+	friend std::istream &operator>>(std::istream &is, Ratio &r) {
+		int num = 0, den = 0;
+		is >> num >> den;
+		Ratio result (num, den);
+		r = result;
+
+		return is;
+	}
+
+	friend std::ostream &operator<<(std::ostream &os, Ratio r) {
+		os << r.num() << '/' << r.den();
+		return os;
+	}
+
+	/// (double) cast
 };
 
 Ratio::Ratio(int num, int den) {
@@ -25,24 +75,34 @@ Ratio::Ratio(int num, int den) {
 	den_ = den / a;
 }
 
-Ratio operator+(Ratio r1, Ratio r2) {
-	return Ratio(
-		r1.num() * r2.den() + r2.num() * r1.den(),
-		r1.den() * r2.den());
-}
-
-std::ostream &operator<<(std::ostream &os, Ratio r) {
-	os << r.num() << '/' << r.den();
-	return os;
-}
+//Ratio operator+(Ratio r1, Ratio r2) {
+//	return Ratio(
+//		r1.num() * r2.den() + r2.num() * r1.den(),
+//		r1.den() * r2.den());
+//}
 
 int main() {
 	Ratio r1(1, 10);
 	Ratio r2(5, 10);
+	////>> In operator test - Passed
+	//Ratio r0(1,1);
+	//std::cin >> r0;
+	//std::cout << r0;
+
+	///* Operator test - Passed
+	Ratio result = r1 * r2;  //expected 5, 100 => 1 /20
+	/// /Operator test - Passed
+	Ratio result2 = r1 / r2; //Expected 1 / 5
+	/// Operator += test - Passed
+	Ratio result3 (1, 1); 
+	result3 += r1; //Expecter 11/10
+	
 	Ratio r3 = r1 + r2;
 	std::cout << r1 << '+' << r2 << '=' << r3 << std::endl;
 
+#ifdef _DEBUG
 	int i;
 	std::cin >> i;
+#endif
 }
 
