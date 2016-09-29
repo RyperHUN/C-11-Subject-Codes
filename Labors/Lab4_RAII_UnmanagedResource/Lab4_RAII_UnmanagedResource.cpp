53601c558d5c11063d71a43666a4835817c42534
RyperHUN <akormanyzsolt@gmail.com>
2016-09-29 13:26:42 +0200

    Lab4_Fileptr

---
+++ Labors/Lab4_RAII_UnmanagedResource/Lab4_RAII_UnmanagedResource.cpp
@@ -0,0 +1,20 @@
+// Lab4_RAII_UnmanagedResource.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+	/*FilePtr fp;
+	fp = fopen("hello.txt", "wt");
+	fprintf(fp, "Hello Vilag");*/
+
+#ifdef _DEBUG
+	int i;
+	std::cin >> i;
+#endif
+    return 0;
+}
+
