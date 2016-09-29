250d2bfb8a6917d405646b525f15c9d9f6520ded
RyperHUN <akormanyzsolt@gmail.com>
2016-09-29 12:32:12 +0200

    Lab4 Funarg problems added

--- Labors/Lab4_FunargProblems/Lab4_FunargProblems.cpp
+++ Labors/Lab4_FunargProblems/Lab4_FunargProblems.cpp
@@ -2,10 +2,64 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <cmath>
 
+class Heron
+{
+	double num = 0;
+
+	bool good_enough (double guess)
+	{
+		return fabs (guess * guess - num) < 0.001;
+	}
+
+	double improve (double guess)
+	{
+		return (guess + num/guess)/2.0;
+	}
+public:
+	double get(double x)
+	{
+		num = x;
+		double guess = 1.0;
+		while (!good_enough(guess))
+			guess = improve(guess);
+
+		return guess;
+	}
+};
+
+class Derival
+{
+	using DoubleFunctionPtr = double (*) (double);
+	DoubleFunctionPtr fv;
+	double dx = 0.001;
+public:
+	Derival (DoubleFunctionPtr fv) 
+		: fv(fv)
+	{}
+
+	double operator()(double x)
+	{
+		return (fv(x+dx) - fv(x)) / dx;
+	}
+};
 
 int main()
 {
-    return 0;
+//Downwards funargot megoldja egy valtozo, hiszen a fuggvenyeknek van *this pointere!
+    Heron h;
+	std::cout << h.get(2.0) << std::endl;
+//Upwards funarg
+	Derival my_cos = Derival{sin};
+	for (double f = 0; f < 3.1415; f += 1)
+		std::cout << std::setw (20) << my_cos(f) - cos(f) << std::endl; //Nagyon kicsi szam = jo!
+
+#ifdef _DEBUG
+	int i;
+	std::cin >> i;
+#endif
 }
 
