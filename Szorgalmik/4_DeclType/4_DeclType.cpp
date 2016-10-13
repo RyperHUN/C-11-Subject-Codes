2787491bbd58b9ce6badfb5a891c0833984abf15
RyperHUN <akormanyzsolt@gmail.com>
2016-10-13 11:37:16 +0200

    Szorgalmi Decltype more info

--- Szorgalmik/4_DeclType/4_DeclType.cpp
+++ Szorgalmik/4_DeclType/4_DeclType.cpp
@@ -6,15 +6,31 @@
 #include <vector>
 #include <iostream>
 
+//decltype(*begin) == int&
+//auto = *begin; // Ekkor auto == int
+
 template <typename ITER>
 auto find_min(ITER begin, ITER end) -> decltype(*begin) {
 	if (begin == end)
 		throw "ures";
-	decltype(*begin) min = *begin;
+	auto min = *begin;
 	while (++begin != end)
 		if (*begin < min)
 			min = *begin;
 	return min;
+}
+
+class iterTest
+{
+	int data;
+public:
+	int& operator*() { return data; }
+};
+
+int& test(iterTest a)
+{
+	iterTest a2 = a;
+	return *a2;
 }
 
 using namespace std;
@@ -23,8 +39,19 @@ int main()
 {
 	vector<int> vec = {10,2,3,1,4,5};
 
-	cout << find_min(vec.begin(), vec.end());
+	cout << find_min(vec.begin(), vec.end()) << endl;
 
+	for(int num : vec)
+		cout << num << "-";
+	cout << endl;
+
+	int c = 2;
+	int& cRef = c;
+	auto isRef = cRef;
+	isRef = 5;
+	cout << isRef << "-" << c;
+
+	test(iterTest{});
 
 	int i;
 	cin >> i;
