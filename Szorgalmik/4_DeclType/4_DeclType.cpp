74cd48dac1d2d05a0035ff0fafea052a835e2007
RyperHUN <akormanyzsolt@gmail.com>
2016-10-13 11:06:06 +0200

    Decltype corrected var

--- Szorgalmik/4_DeclType/4_DeclType.cpp
+++ Szorgalmik/4_DeclType/4_DeclType.cpp
@@ -3,9 +3,31 @@
 
 #include "stdafx.h"
 
+#include <vector>
+#include <iostream>
+
+template <typename ITER>
+auto find_min(ITER begin, ITER end) -> decltype(*begin) {
+	if (begin == end)
+		throw "ures";
+	decltype(*begin) min = *begin;
+	while (++begin != end)
+		if (*begin < min)
+			min = *begin;
+	return min;
+}
+
+using namespace std;
 
 int main()
 {
+	vector<int> vec = {10,2,3,1,4,5};
+
+	cout << find_min(vec.begin(), vec.end());
+
+
+	int i;
+	cin >> i;
     return 0;
 }
 
