b248ed812c50730c266fdd859047ce960c24fe12
RyperHUN <akormanyzsolt@gmail.com>
2016-10-13 12:29:43 +0200

    Added global operator new/delete

--- Labors/Lab6_GlobalOperatorNew/Lab6_GlobalOperatorNew.cpp
+++ Labors/Lab6_GlobalOperatorNew/Lab6_GlobalOperatorNew.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include <new>
+#include <iostream>
 
+using namespace std;
+
+void * operator new(size_t size)
+{
+	void* ptr = malloc (size);
+	cout << "Allocated " << size << " bytes at 0x" << ptr << endl;
+	
+	return ptr;
+}
+
+void operator delete(void * ptr) noexcept
+{
+	cout << "freed memory at 0x" << ptr << endl;
+	free (ptr);
+}
 
 int main()
 {
+	char* c = (char*) ::operator new (1);
+	*c = 'b';
+
+	cout << *c << endl;
+
+	::operator delete (c);
+
+
+#ifdef _DEBUG
+	int i;
+	std::cin >> i;
+#endif
     return 0;
 }
 
