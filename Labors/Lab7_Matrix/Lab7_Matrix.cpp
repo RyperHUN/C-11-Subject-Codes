0fa58b0446ad0c13d6220bd66267bdf840b2292e
RyperHUN <akormanyzsolt@gmail.com>
2016-10-20 13:13:36 +0200

    Lab7 Matrix init

---
+++ Labors/Lab7_Matrix/Lab7_Matrix.cpp
@@ -0,0 +1,20 @@
+// Lab7_Matrix.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+class Matrix {
+private:
+	size_t w_, h_;
+	double *data_;
+public:
+	Matrix (size_t w, size_t h) 
+		: w_(w), h_(h)
+	{}
+};
+
+int main()
+{
+    return 0;
+}
+
