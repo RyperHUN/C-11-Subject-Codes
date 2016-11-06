17583726e829a34da0ed13f931aad5698ae5ac7c
RyperHUN <akormanyzsolt@gmail.com>
2016-11-06 16:43:41 +0100

    HomeWork InputHandler added

---
+++ Homeworks/InputHandler/Skeleton/Main.cpp
@@ -0,0 +1,157 @@
+﻿#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#if defined(__APPLE__)
+#include <OpenGL/glew.h>
+#include <OpenGL/freeglut.h>
+#else
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#include <windows.h>
+#endif
+#include <GL/glew.h>		// must be downloaded 
+#include <GL/freeglut.h>	// must be downloaded unless you have an Apple
+#endif
+
+const unsigned int windowWidth = 600, windowHeight = 600;
+
+//Verziokezelés tesztü
+// OpenGL major and minor versions
+int majorVersion = 3, minorVersion = 0;
+
+void getErrorInfo(unsigned int handle) {
+	int logLen;
+	glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &logLen);
+	if (logLen > 0) {
+		char * log = new char[logLen];
+		int written;
+		glGetShaderInfoLog(handle, logLen, &written, log);
+		printf("Shader log:\n%s", log);
+		delete log;
+	}
+}
+
+// check if shader could be compiled
+void checkShader(unsigned int shader, char * message) {
+	int OK;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &OK);
+	if (!OK) {
+		printf("%s!\n", message);
+		getErrorInfo(shader);
+	}
+}
+
+// check if shader could be linked
+void checkLinking(unsigned int program) {
+	int OK;
+	glGetProgramiv(program, GL_LINK_STATUS, &OK);
+	if (!OK) {
+		printf("Failed to link shader program!\n");
+		getErrorInfo(program);
+	}
+}
+
+// Initialization, create an OpenGL context
+void onInitialization() {
+	glViewport(0, 0, windowWidth, windowHeight);
+
+}
+
+
+
+
+//=============================== ====================================EVENTS===================================================================================/
+void onExit() {
+	printf("exit");
+}
+
+// Window has become invalid: Redraw
+void onDisplay() {
+	glClearColor(0, 0, 0, 0);							// background color 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the screen
+
+	glutSwapBuffers();									// exchange the two buffers
+}
+
+
+// Key  ASCII code pressed
+// WASD- Kamera mozgatasa
+// RF - Kamera zoom
+// Space - Csillag focus ON/OFF
+void onKeyboard(unsigned char key, int pX, int pY) {
+	if (key == 'd')
+		printf("d pressed");
+	//if (key == 'd')
+	//	camera.wCx += cameraSpeed;
+	//else if (key == 'a')
+	//	camera.wCx -= cameraSpeed;
+	//else if (key == 'w')
+	//	camera.wCy += cameraSpeed;
+	//else if (key == 's')
+	//	camera.wCy -= cameraSpeed;
+	//else if (key == 'f')
+	//	camera.increaseScale(0.1f, 0.1f);
+	//else if (key == 'r')
+	//	camera.increaseScale(-0.1f, -0.1f);
+	//else if (key == ' ')
+	//	camera.toggleFollow();
+}
+
+// Key of ASCII code released
+void onKeyboardUp(unsigned char key, int pX, int pY) {
+
+}
+
+// Mouse click event
+void onMouse(int button, int state, int pX, int pY) {
+	
+}
+
+// Move mouse with key pressed
+void onMouseMotion(int pX, int pY) {
+}
+
+// Idle event indicating that some time elapsed: do animation here
+void onIdle() {
+	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+	float sec = time / 1000.0f;
+	
+	glutPostRedisplay();					// redraw the scene
+}
+
+// Idaig modosithatod...
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int main(int argc, char * argv[]) {
+	glutInit(&argc, argv);
+
+	glutInitContextVersion(majorVersion, minorVersion);
+	glutInitWindowSize(windowWidth, windowHeight);				// Application window is initially of resolution 600x600
+	glutInitWindowPosition(100, 100);							// Relative location of the application window
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);  // 8 bit R,G,B,A + double buffer + depth buffer
+	glutCreateWindow(argv[0]);
+	glewExperimental = true;
+	glewInit();
+
+	printf("GL Vendor    : %s\n", glGetString(GL_VENDOR));
+	printf("GL Renderer  : %s\n", glGetString(GL_RENDERER));
+	printf("GL Version (string)  : %s\n", glGetString(GL_VERSION));
+	glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+	glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+	printf("GL Version (integer) : %d.%d\n", majorVersion, minorVersion);
+	printf("GLSL Version : %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	onInitialization();
+
+	glutDisplayFunc(onDisplay);                // Register event handlers
+	glutMouseFunc(onMouse);
+	glutIdleFunc(onIdle);
+	glutKeyboardFunc(onKeyboard);
+	glutKeyboardUpFunc(onKeyboardUp);
+	glutMotionFunc(onMouseMotion);
+
+	glutMainLoop();
+	onExit();
+	return 1;
+}
