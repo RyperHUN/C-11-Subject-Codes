2953f4841e591968fa969a8f145e6d695a1dea68
RyperHUN <akormanyzsolt@gmail.com>
2016-11-09 16:11:17 +0100

    Added hanoi2 with capture list var, not yet working

--- Szorgalmik/6_LambdaHanoi/6_LambdaHanoi.cpp
+++ Szorgalmik/6_LambdaHanoi/6_LambdaHanoi.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
+#include <functional>
 
 class FvTor {
 public:
@@ -18,7 +19,7 @@ public:
 };
 
 template <typename T>
-void hanoi (int n, char honnan, char seged, char hova, T fv)
+void hanoi (int n, char honnan, char seged, char hova,T fv)
 {
 	if (n == 0)
 		return;
@@ -27,12 +28,26 @@ void hanoi (int n, char honnan, char seged, char hova, T fv)
 	hanoi (n - 1, seged, honnan, hova, fv);
 }
 
+
+void hanoi2 (int n, char honnan, char seged, char hova, std::function<void (char, char)> fv)
+{
+	if (n == 0)
+		return;
+	hanoi2 (n - 1, honnan, hova, seged, fv);
+	fv (honnan, hova);
+	hanoi2 (n - 1, seged, honnan, hova, fv);
+}
+
 int main (void)
 {
 	int counter = 0;
 	hanoi (4, 'A', 'B', 'C', 
 		   [&counter] (char a, char b) {
 				std::cout << "Lepes:" << counter++ << " Rakd a korongot " << a << " oszloprol " << b << " oszlopra\n";
+	});
+	hanoi2 (4, 'A', 'B', 'C',
+			[count = 0](char a, char b) mutable {
+		std::cout << "2/Lepes:" << count++ << " Rakd a korongot " << a << " oszloprol " << b << " oszlopra\n";
 	});
 	return 0;
 }
