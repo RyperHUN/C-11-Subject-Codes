5e8d8d711f4d6510543b194879d5709dafe99698
RyperHUN <akormanyzsolt@gmail.com>
2016-11-10 12:31:14 +0100

    Added double lambda

--- Labors/Lab10_Transform/Lab10_Transform.cpp
+++ Labors/Lab10_Transform/Lab10_Transform.cpp
@@ -9,11 +9,29 @@
 #include <algorithm>
 #include <functional>
 
-int main() {
-	std::vector<double> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	//std::function<double(double)> SQRT = sqrt;
-	//std::transform(v.begin(), v.end(), v.begin(), sqrt);
+double doubler (double x)
+{
+	return x *2;
+}
 
-	for (auto i : v)
-		std::cout << i << std::endl;
+int main() {
+	{
+		std::vector<double> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		std::function<double(double)> SQRT = static_cast<double(*)(double)>(sqrt);
+		std::transform(v.begin(), v.end(), v.begin(), SQRT);
+
+		for (auto i : v)
+			std::cout << i << std::endl;
+	}
+	std::cout << "---------------------" << std::endl;
+	{
+		std::vector<double> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		//std::transform(v.begin(), v.end(), v.begin(), doubler);
+		std::transform (v.begin(), v.end (), v.begin (), [](int x){return x *2;});
+
+		for (auto i : v)
+			std::cout << i << std::endl;
+	}
+
 }
