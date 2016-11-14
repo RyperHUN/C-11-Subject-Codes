e97e1c1c724e3c0b9d59a2471b80f81ddb8b6710
RyperHUN <akormanyzsolt@gmail.com>
2016-11-14 18:49:56 +0100

    Added demo code

--- Szorgalmik/Projects/7_TesterImprovement/7_TesterImprovement.cpp
+++ Szorgalmik/Projects/7_TesterImprovement/7_TesterImprovement.cpp
@@ -100,46 +100,148 @@ public:
 	}
 };
 
+class NewTester {
+	
+public:
+	using Constraint = std::function<bool ()>;
+	using Activity = std::function<void ()>;
+
+
+private:
+	
+	
+public:
+	
+	class ContainerClass {
+		using Container = std::vector<VALUETYPE>;
+		using VariableValue = std::function<VALUETYPE ()>;
+
+		template <typename VALUETYPE>
+		void add {
+
+		}
+
+		using Iterator = typename Container::const_iterator;
+		struct Variable {
+			Container values_;
+			Iterator current_;
+		};
+		std::vector<Variable> variables_;
+		std::vector<Constraint> constraints_;
+		VALUETYPE operator ()	{
+
+		}
+	};
+	template <typename T>
+	std::function<T ()> add_variable (std::initializer_list<T> list)
+	{
+		variables_.push_back (Variable{values_, Iterator{}});
+		size_t pos = variables_.size () - 1;
+		return [this, pos]() -> Any {
+			return *variables_[pos].current_;
+		};
+	}
+
+	/* FeltĂŠtel hozzĂĄadĂĄsa. A feltĂŠtel egy fĂźggvĂŠny, amely igaz ĂŠrtĂŠke
+	* esetĂŠn az aktuĂĄlis ĂĄllapot elfogadhatĂł. */
+	
+	void add_constraint (Constraint values_)
+	{
+		constraints_.push_back (values_);
+	}
+
+	/* Teszteli az Ăśsszes lehetsĂŠges ĂŠrtĂŠket, ĂŠs amikor olyan
+	* ĂĄllapotban vannak az iterĂĄtorok, amik ĂŠpp egy elfogadhatĂł
+	* kombinĂĄciĂłra mutatnak, akkor meghĂ­vja a fĂźggvĂŠnyt. */
+	void solve (Activity do_what)
+	{
+		/* iterĂĄtorok inicializĂĄlĂĄsa */
+		for (auto & v : variables_)
+			v.current_ = v.values_.begin ();
+		/* A Variable-k current iterĂĄtorait mĂĄr mind begin-re
+		* ĂĄllĂ­tottuk, szĂłval most az elejĂŠn ĂĄll az Ăśsszes. */
+		bool end = false;
+		while (!end) {
+			/* JĂł az aktuĂĄlis ĂĄllapot? Ha mindegyik jĂł. */
+			auto satisfied = std::all_of (
+				constraints_.begin (), constraints_.end (),
+				[](Constraint cons) { return cons (); });
+			/* Megfelel ez az ĂĄllapot, ha mindegyik Constraint igazat vĂĄlaszolt. */
+			if (satisfied)
+				do_what ();
+
+			/* KĂśvetkezĹre ugrĂĄs: a digit "teljes ĂśsszeadĂł"ja kĂłdban. */
+			bool carry = true;
+			for (auto & v : variables_) {
+				++v.current_;
+				carry = v.current_ == v.values_.end ();
+				if (carry)
+					v.current_ = v.values_.begin ();
+				else
+					break;
+			}
+			/* Ha vĂŠge lett, ĂŠs mĂŠg mindig carry=true, kĂśrbeĂŠrtĂźnk. */
+			end = carry;
+		}
+	}
+};
+
 
 int main ()
 {
-	Tester<int> t;
+	NewTester t;
 
-	// Baker, Cooper, Fletcher, Miller ĂŠs Smith egy Ăśtemeletes hĂĄz
-	auto baker = t.add_variable ({1, 2, 3, 4, 5});
-	auto cooper = t.add_variable ({1, 2, 3, 4, 5});
-	auto fletcher = t.add_variable ({1, 2, 3, 4, 5});
-	auto miller = t.add_variable ({1, 2, 3, 4, 5});
-	auto smith = t.add_variable ({1, 2, 3, 4, 5});
-
-	// kĂźlĂśnbĂśzĹ emeletein laknak.
-	auto guys ={baker, cooper, fletcher, miller, smith};
-	t.add_constraint ([=]() -> bool {
-		std::set<int> s;
-		for (auto f : guys)
-			s.insert (f ());
-		return s.size () == guys.size ();
+	/* Bugyuta példa: kilistázza azokat a (szám;szó) párokat,
+	* amelyeknél hossz(szó) = szám */
+	auto size = t.add_variable ({1,2,3,4,5});
+	auto word = t.add_variable ({"alma", "korte", "barack"});
+	t.add_constraint ([=] {
+		return size () == (int) strlen (word ());
 	});
-	// Baker nem a legfĂślsĹ emeleten lakik,
-	t.add_constraint ([=]()  mutable -> bool { return baker () != 5; });
-	// Cooper pedig nem az alsĂł szinten.
-	t.add_constraint ([=] { return cooper () != 1; });
-	// Fletcher lakhelye sem a legalsĂł szinten van, de nem is a legfĂślsĹn.
-	t.add_constraint ([=] { return fletcher () != 1 && fletcher () != 5; });
-	// Miller magasabban lakik, mint Cooper. 
-	t.add_constraint ([=] { return miller () > cooper (); });
-	// Smith nem Fletcherrel szomszĂŠdos emeleten lakik,
-	t.add_constraint ([=] { return abs (smith () - fletcher ()) != 1; });
-	// ahogy Cooper ĂŠs Fletcher sem emelet-szomszĂŠdok.
-	t.add_constraint ([=] { return abs (cooper () - fletcher ()) != 1; });
-
-	// A kĂŠrdĂŠs: melyik emelet kiĂŠ?
-	auto print_all = [=] {
-		std::cout << "Baker " << baker () << std::endl
-			<< "Cooper " << cooper () << std::endl
-			<< "Fletcher " << fletcher () << std::endl
-			<< "Miller " << miller () << std::endl
-			<< "Smith " << smith () << std::endl;
-	};
-	t.solve (print_all);
+	t.solve ([=] {
+		std::cout << "strlen(" << word () << ") = " << size () << std::endl;
+	});
 }
+
+//int main ()
+//{
+//	Tester<int> t;
+//
+//	// Baker, Cooper, Fletcher, Miller ĂŠs Smith egy Ăśtemeletes hĂĄz
+//	auto baker = t.add_variable ({1, 2, 3, 4, 5});
+//	auto cooper = t.add_variable ({1, 2, 3, 4, 5});
+//	auto fletcher = t.add_variable ({1, 2, 3, 4, 5});
+//	auto miller = t.add_variable ({1, 2, 3, 4, 5});
+//	auto smith = t.add_variable ({1, 2, 3, 4, 5});
+//
+//	// kĂźlĂśnbĂśzĹ emeletein laknak.
+//	auto guys ={baker, cooper, fletcher, miller, smith};
+//	t.add_constraint ([=]() -> bool {
+//		std::set<int> s;
+//		for (auto f : guys)
+//			s.insert (f ());
+//		return s.size () == guys.size ();
+//	});
+//	// Baker nem a legfĂślsĹ emeleten lakik,
+//	t.add_constraint ([=]()  mutable -> bool { return baker () != 5; });
+//	// Cooper pedig nem az alsĂł szinten.
+//	t.add_constraint ([=] { return cooper () != 1; });
+//	// Fletcher lakhelye sem a legalsĂł szinten van, de nem is a legfĂślsĹn.
+//	t.add_constraint ([=] { return fletcher () != 1 && fletcher () != 5; });
+//	// Miller magasabban lakik, mint Cooper. 
+//	t.add_constraint ([=] { return miller () > cooper (); });
+//	// Smith nem Fletcherrel szomszĂŠdos emeleten lakik,
+//	t.add_constraint ([=] { return abs (smith () - fletcher ()) != 1; });
+//	// ahogy Cooper ĂŠs Fletcher sem emelet-szomszĂŠdok.
+//	t.add_constraint ([=] { return abs (cooper () - fletcher ()) != 1; });
+//
+//	// A kĂŠrdĂŠs: melyik emelet kiĂŠ?
+//	auto print_all = [=] {
+//		std::cout << "Baker " << baker () << std::endl
+//			<< "Cooper " << cooper () << std::endl
+//			<< "Fletcher " << fletcher () << std::endl
+//			<< "Miller " << miller () << std::endl
+//			<< "Smith " << smith () << std::endl;
+//	};
+//	t.solve (print_all);
+//}
