37aab389b907ac54ee34e795c3cd50756d3935c2
RyperHUN <akormanyzsolt@gmail.com>
2016-11-20 19:42:08 +0100

    Added int type for print

--- Szorgalmik/8_MetaProgrammingPrint/8_MetaProgrammingPrint.cpp
+++ Szorgalmik/8_MetaProgrammingPrint/8_MetaProgrammingPrint.cpp
@@ -2,8 +2,52 @@
 //
 //#include "stdafx.h"
 
+#include <type_traits>
+#include <iostream>
+
+template <typename T>
+struct IsNumber {
+	static constexpr bool value = false;
+};
+
+template <>
+struct IsNumber<int> {
+	static constexpr bool value = true;
+};
+
+template <>
+struct IsNumber<double> {
+	static constexpr bool value = true;
+};
+
+template <>
+struct IsNumber<float> {
+	static constexpr bool value = true;
+};
+
+
+template <typename T>
+typename std::enable_if<IsNumber <T>::value>::type 
+print (T val)
+{
+	std::cout << val << " ";
+}
+
+//print <typename T>
+//typename std::enable_if<std::is
+
+
+	
 int main()
 {
+	std::cout << "Number test:" << std::endl;
+	print (5);
+	print (5.1);
+	print (5.1f);
+
+	std::cout << std::endl << "";
+
+
     return 0;
 }
 
