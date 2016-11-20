1a17ce823d2b0f29b0ebb122fabb44217452edc9
RyperHUN <akormanyzsolt@gmail.com>
2016-11-20 20:05:13 +0100

    Added string printing

--- Szorgalmik/8_MetaProgrammingPrint/8_MetaProgrammingPrint.cpp
+++ Szorgalmik/8_MetaProgrammingPrint/8_MetaProgrammingPrint.cpp
@@ -4,6 +4,7 @@
 
 #include <type_traits>
 #include <iostream>
+#include <string>
 
 template <typename T>
 struct IsNumber {
@@ -41,6 +42,23 @@ struct IsChar<char16_t> {
 };
 
 template <typename T>
+struct IsString {
+	static constexpr bool value = false;
+};
+
+template <>
+struct IsString<const char*> {
+	static constexpr bool value = true;
+};
+
+template <>
+struct IsString<std::string> {
+	static constexpr bool value = true;
+};
+
+
+
+template <typename T>
 typename std::enable_if<IsNumber <T>::value>::type 
 print (T val)
 {
@@ -57,6 +75,13 @@ print(T val)
 	std::cout << "'" << val << "' ";
 }
 
+template <typename T>
+typename std::enable_if<IsString<T>::value>::type
+print(T val)
+{
+	std::cout << "\"" << val << "\" ";
+}
+
 
 //print <typename T>
 //typename std::enable_if<std::is
@@ -70,11 +95,16 @@ int main()
 	print (5.1);
 	print (5.1f);
 
-	std::cout << std::endl << "Char test:" << std::endl;
+	std::cout << std::endl << std::endl << "Char test:" << std::endl;
 	print('c');
 	print(static_cast<char16_t>('b'));
 	
-	std::cout << std::endl << "String test:" << std::endl;
+	std::cout << std::endl << std::endl <<"String test:" << std::endl;
+
+	print("Easy game");
+	std::string str("test");
+	print(str);
+
 
     return 0;
 }
