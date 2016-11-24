cefec2a7b794d9ad69b522d5c8b919101db08435
RyperHUN <akormanyzsolt@gmail.com>
2016-11-24 11:26:18 +0100

    Working own std::ref

--- Szorgalmik/9_StdRef/9_StdRef.cpp
+++ Szorgalmik/9_StdRef/9_StdRef.cpp
@@ -2,7 +2,8 @@
 //
 
 #include <iostream>
-#include <algorithm>
+#include <type_traits>
+//#include <algorithm>
 
 class IntPrinter {
 private:
@@ -13,6 +14,36 @@ public:
 	}
 };
 
+template< class T >
+class reference_wrapper {
+	static_assert (std::is_class<T>::value, "Only accepts Objects");
+	T& _val;
+public:
+	reference_wrapper (T &val)
+		:_val (val) {}
+
+	/*operator T& () {return _val;}*/
+	template< class... Types>
+	void operator() (Types ... types) {
+		_val (types...);
+	}
+};
+
+template <typename T>
+reference_wrapper<T> ref (T& val)
+{
+	return reference_wrapper<T>(val);
+}
+
+template<class InputIt, class UnaryFunction>
+UnaryFunction for_each(InputIt first, InputIt last, UnaryFunction f)
+{
+	for (; first != last; ++first) {
+		f(*first);
+	}
+	return f;
+}
+
 
 
 int main()
@@ -20,8 +51,14 @@ int main()
 	int arr1[] = { 5, 9, 2, 4 };
 	int arr2[] = { 5, 8, 4, 7, 9 };
 	IntPrinter p;
-	std::for_each(std::begin(arr1), std::end(arr1), std::ref(p));     /* 1. 2. 3. 4. */
-	std::for_each(std::begin(arr2), std::end(arr2), std::ref(p));     /* 5. 6. 7. 8. 9. */
+	reference_wrapper<IntPrinter> wrappedIntPrinter = ref (p);
+	//ref (1); // Compile time error
+
+	//wrappedIntPrinter (); // Compile time error
+	wrappedIntPrinter (1);
+	wrappedIntPrinter (2);
+	for_each(std::begin(arr1), std::end(arr1), ref(p));     /* 1. 2. 3. 4. */
+	for_each(std::begin(arr2), std::end(arr2), p);     /* 5. 6. 7. 8. 9. */
 
     return 0;
 }
