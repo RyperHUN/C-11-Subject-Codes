51466bbc735b8d0b0c3e70cfedf5e51f3f18370e
RyperHUN <akormanyzsolt@gmail.com>
2016-11-24 13:50:05 +0100

    Added fibonacci meta

--- Labors/Lab11_FibonacciMeta/Lab11_FibonacciMeta.cpp
+++ Labors/Lab11_FibonacciMeta/Lab11_FibonacciMeta.cpp
@@ -18,13 +18,29 @@ int fib (int n) {
 }
 
 
+template <int N>
+struct Fib {
+	static constexpr int value = Fib<N-1>::value + Fib<N-2>::value;
+};
+
+template <>
+struct Fib<0> {
+	static constexpr int value = 0;
+};
+template <>
+struct Fib<1> {
+	static constexpr int value = 1;
+};
+
 
 int main()
 {
-	for (int i = 0; i < 41; i++) {
+	/*for (int i = 0; i < 41; i++) {
 		calculatedFib[i] = fib(i);
 		std::cout << "fib(" << i << ") = " << calculatedFib[i] << std::endl;
-	}
+	}*/
+
+	std::cout << Fib<40>::value << std::endl;
     return 0;
 }
 
