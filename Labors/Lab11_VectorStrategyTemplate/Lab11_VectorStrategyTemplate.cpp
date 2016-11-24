fcc7f3a2fc35bb65e0b17bfbbad6fe7f5eab2dfc
RyperHUN <akormanyzsolt@gmail.com>
2016-11-24 12:49:44 +0100

    Only accepts derived of vector + Added vector param

--- Labors/Lab11_VectorStrategyTemplate/Lab11_VectorStrategyTemplate.cpp
+++ Labors/Lab11_VectorStrategyTemplate/Lab11_VectorStrategyTemplate.cpp
@@ -2,10 +2,31 @@
 //
 
 #include <iostream>
+#include <type_traits>
+
+struct Strategy {
+
+};
 
 template <typename T>
+struct IsStrategy {
+	static constexpr bool value = false;
+};
+
+template <>
+struct IsStrategy <Strategy> {
+	static constexpr bool value = true;
+};
+
+//Maybe static asser + is base of
+template <
+	      typename T, 
+		  class StrategyType /*,
+		  typename Dummy     = typename std::enable_if< IsStrategy<StrategyType>::value>::type*/  //Only accepts Strategy
+		 > 
 class Vector {
 private:
+	static_assert (std::is_base_of <Strategy, StrategyType>::value, "Only accepts Strategy types");
 	size_t size;
 	T* data;
 
@@ -21,11 +42,38 @@ public:
 	}
 
 	T& operator[](size_t idx) {
+		StrategyType::IndexChecker (*this, size, idx);
+
 		return data[idx];
 	}
 };
 
+class StrategyNull : Strategy
+{
+public:
+	template <typename T>
+	static void IndexChecker(Vector<T, StrategyNull>& /*vec*/, size_t /*size*/, size_t /*index*/) {}
+};
+
+class StrategyException : Strategy
+{
+public:
+	template <typename T>
+	static void IndexChecker(Vector<T, StrategyException>& vec, size_t size, size_t index) {
+		if (size <= index)
+			throw std::out_of_range("size is bigger than index");
+	}
+};
+
 int main() {
-	Vector<int> v1(10);
+	try {
+		//Vector<int,int> test(10);
+		Vector<int, StrategyNull> v1(10);
+		v1 [11]; // Nothing
+		Vector<int, StrategyException> vExc (10);
+		vExc [11];
+	} catch (std::exception exc) {
+		std::cout << "ERROR " << exc.what() << std::endl;
+	}
 }
 
