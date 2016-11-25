ac9adb9333a18a901e03ce09336f2f1cd634e81b
RyperHUN <akormanyzsolt@gmail.com>
2016-11-25 21:57:00 +0100

    Added IsFunction

--- Szorgalmik/11_TypeTraitCompetition/11_TypeTraitCompetition.cpp
+++ Szorgalmik/11_TypeTraitCompetition/11_TypeTraitCompetition.cpp
@@ -24,6 +24,25 @@ public:
 };
 
 
+
+//IsFunction<T>::value: függvény - e, pl. int(bool) függvény, mert int f(bool);
+template <typename FUNC>
+class IsFunction;
+
+template <typename RET, typename ... ARGS>
+class IsFunction<RET(ARGS...)> {
+private:
+
+public:
+	static constexpr bool value = true;
+};
+
+template <typename FUNC>
+class IsFunction {
+public:
+	static constexpr bool value = false;
+};
+
 } // NS Ryper
 
 
@@ -34,6 +53,10 @@ struct Derived : public Base {};
 using namespace Ryper;
 int main()
 {
+	std::cout << "IsFunction test:" << std::endl;
+	std::cout << IsFunction<int>::value <<std::endl;
+	std::cout << IsFunction<int()>::value << std::endl;
+	std::cout << std::endl << "--------------------------" << std::endl;
 	std::cout << "IsSame test:" << std::endl;
 	std::cout << "int == double " << IsSame<int,double>::value << std::endl;
 	std::cout << "Base == Derived " << IsSame<Base,Derived>::value << std::endl;
